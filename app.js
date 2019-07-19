095c5ab048594306ac48c01b1bbc39937ee5e225
Alan Lawrance <alan.lawrance@gmail.com>
2019-07-19 06:03:24 -0700

    Use of end event to determine when file download is complete

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var fs = require('fs');
-var wget = require('node-wget');
+var http = require('http');
 var execSync = require('child_process').execSync;
 var util = require('util');
 
@@ -21,7 +21,7 @@ var steamcmd = "";
 const port = process.env.port || 5000
 
 process.on('uncaughtException', function (err) {
-console.log(err); //Send some notification about the error
+console.log(err);
 process.exit(1); });
 
 app.post('/', function (req, res) {
@@ -29,6 +29,8 @@ app.post('/', function (req, res) {
   if (fs.existsSync(config_filename)) {
     parse_config(config_filename);
     process_href(req.body.links.artifacts[0].files[0].href);
+  } else {
+    console.log('%s not found so Build Success Event ignored', config_filename);
   }
 });
 
@@ -36,33 +38,47 @@ function process_href(href)
 {
   remove_build(content_dir);
   create_directory(content_dir);
-  wget({
-	url: href, 
-        dest: temp_zip_filename,
-        timeout: 3600000       // 1 hour
-    },
-    wget_completed
-  );
+  remove_file(temp_zip_filename);
+  download_href(href, temp_zip_filename); 
 }
 
-function wget_completed(error, response, body)
+function download_href(url, dest)
+{
+  try {
+    var file = fs.createWriteStream(dest);
+  } catch(error) {
+    console.log('Create %s failed with error %s', dest, error.message);
+    process.exit(1);
+  }
+
+  http.get(url, function(res) {
+    res.on('data', function(data) {
+      file.write(data);
+    }).on('end', function() {
+      console.log('Downloaded build to %s', dest);
+      file.end();
+      download_completed(dest);
+    });
+  });
+}
+
+function download_completed(dest)
 {
   if (error) {
     console.log(error);
   } else {
-    decompress_build(temp_zip_filename, content_dir);
+    decompress_build(dest, content_dir);
     steam_deploy();
-    fs.unlinkSync(temp_zip_filename);
   }
 }
 
-function decompress_build(input_filename, output_path)
+function decompress_build(filename, output_path)
 {
   console.log('Decompressing build...');
   try {
-    execSync(`unzip -o ${temp_zip_filename} -d ${content_dir}`);
+    execSync(`unzip -o ${filename} -d ${content_dir}`);
   } catch (error) {
-    console.log('Unzip failed');
+    console.log('Unzip failed with error %s', error.message);
     process.exit(1);
   }
 }
@@ -73,7 +89,7 @@ function steam_deploy()
   try {
     execSync(`${steamcmd} +login ${username} ${password} +run_app_build ${appvdf} +quit`);
   } catch (error) {
-    console.log('Upload failed');
+    console.log('Upload failed with error %s', error.message);
     process.exit(1);
   }
 
@@ -83,7 +99,7 @@ function steam_deploy()
 function parse_config(filename)
 {
   var data = fs.readFileSync(filename, 'utf8');
-  var lines = data.split("\n");
+  var lines = data.split('\n');
   username = lines[0].trim();
   password = lines[1].trim();
   content_dir = lines[2].trim();
@@ -98,6 +114,13 @@ function create_directory(path)
   }
 }
 
+function remove_file(path)
+{
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path);
+  }
+}
+
 function remove_build(path)
 {
   delete_folder_recursive(path);
@@ -107,7 +130,7 @@ function delete_folder_recursive(path)
 {
   if( fs.existsSync(path) ) {
     fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
+      var curPath = path + '/' + file;
       if(fs.lstatSync(curPath).isDirectory()) { // recurse
         delete_folder_recursive(curPath);
       } else { // delete file
@@ -119,5 +142,5 @@ function delete_folder_recursive(path)
 };
 
 app.listen(port, function () {
-  console.log("Listening on port %s", port);
+  console.log('Listening on port %s', port);
 });
